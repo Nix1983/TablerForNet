8ac1b8a87f5ccc891518650007ae1f98eceec863
Nix <pazi@aol.at>
2023-08-24 23:55:16 +0200

    #1 Change badge namespace

--- Components/Badges/Badge.razor.cs
+++ Components/Badges/Badge.razor.cs
@@ -1,4 +1,4 @@
-﻿namespace TablerForNet.Components.Badges
+﻿namespace TablerForNet.Components
 {
     public enum BadgeShape
     {
@@ -11,23 +11,4 @@
         Default,
         Large
     }
-
-    public partial class Badge : TablerBaseComponent
-    {
-        [Parameter] public BadgeShape Shape { get; set; }
-        [Parameter] public BadgeSize Size { get; set; } = BadgeSize.Default;
-        protected string HtmlTag => "span";
-
-        protected override string ClassNames => ClassBuilder
-            .Add("badge")
-            .Add(TextColor.GetColorClass("text"))
-            .Add("badge-default")
-            .AddCompare("badge-md", Size, BadgeSize.Default)
-            .Add(BackgroundColor.GetColorClass("bg", ColorType.Default))
-            .AddCompare("badge-pill", Shape, BadgeShape.Pill)
-            .AddIf("cursor-pointer", OnClick.HasDelegate)
-            .ToString();
-    }
-
-
 }
