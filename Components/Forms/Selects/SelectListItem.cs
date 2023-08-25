5f871c299e9ebcf033066cb17d168321b5f4656e
Nix <nixgithub@outlook.com>
2023-08-25 12:25:06 +0200

    #1 Change namespace Forms

--- Components/Forms/Selects/SelectListItem.cs
+++ Components/Forms/Selects/SelectListItem.cs
@@ -1,6 +1,6 @@
-﻿namespace TablerForNet.Components.Forms.Selects
+﻿namespace TablerForNet.Components
 {
-    public class ListItem<TItem, TValue>
+    public class SelectListItem<TItem, TValue>
     {
         public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Text { get; set; }
