4806bff2351c9da70aae234f8780fd49962db1a2
Nix <nixgithub@outlook.com>
2023-09-03 16:26:07 +0200

    Add class NavBarBase | Cleaner code

---
+++ Components/Navbars/NavbarBase.cs
@@ -0,0 +1,8 @@
+﻿namespace TablerForNet.Components
+{
+    public abstract class NavbarBase :ComponentBase
+    {
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+    }
+}
