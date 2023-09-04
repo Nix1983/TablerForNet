be49567cc78d1f3dbdc06aef004d04c21eaf6c33
Nix <nixgithub@outlook.com>
2023-09-04 16:47:56 +0200

    Add PageLauyout Enum extension

--- Components/Pages/PageLayout.cs
+++ Components/Pages/PageLayout.cs
@@ -7,4 +7,22 @@ namespace TablerForNet.Components
         Fluid,
         None
     }
+
+    public static class PageLayoutExtensions
+    {
+        public static string ToCssClass(this PageLayout layout)
+        {
+            switch (layout)
+            {
+                case PageLayout.Boxed:
+                    return "layout-boxed";
+                case PageLayout.Fluid:
+                    return "layout-fluid";
+                case PageLayout.None:
+                    return "";  // oder eine entsprechende CSS-Klasse für "None"
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
 }
